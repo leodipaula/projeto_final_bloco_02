991ea7d65b03b66e45b2f36a4253822020c16321
leodipaula <leofernandes9@gnail.com>
2025-05-05 09:20:49 -0300

    Estrutura inicial

---
+++ src/main/java/com/leodipaula/e_farma/EFarmaApplication.java
@@ -0,0 +1,13 @@
+package com.leodipaula.e_farma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EFarmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EFarmaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leodipaula/e_farma/EFarmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leodipaula.e_farma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EFarmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
